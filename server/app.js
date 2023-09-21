d6b486a54a3760d0866f683cae53d88e7ab09ac4
dattatreyav30 <dattatreya361@gmail.com>
2023-09-21 13:01:50 +0530

    database ready

--- server/app.js
+++ server/app.js
@@ -6,11 +6,15 @@ const bodyParser = require("body-parser");
 
 const cors = require("cors");
 
+const sequelize = require("./util/database")
+
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+sequelize.sync();
+ 
 app.listen(6666, () => {
   console.log("listening on 6666");
 });
